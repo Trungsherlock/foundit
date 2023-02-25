f78c7a76d5191b83d0b5358b3992a50c11a4c13a
Châu Anh Phạm <chauanhpham@vl965-172-31-168-108.wireless.umass.edu>
2023-02-25 17:17:10 -0500

    Details

---
+++ src/components/templates/ideas-details/IdeasDetails.tsx
@@ -0,0 +1,99 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, FC } from "react";
+import cn from "classnames";
+import styles from "./IssueDetails.module.sass";
+//import {UserItem} from "../../modules/userItem";
+import { IIdeasDetails } from "./types";
+//import useUserByUid from "../../../hooks/useUserByUid";
+
+
+const navLinks = ["By"];
+
+const idea = {
+    id: "One",
+    title: "Caticorn",
+    type: [],
+    description: "Paimon is food",
+    feature: "none",
+    createdAt: "today",
+    updatedAt: "",
+    authorId: "hehe",
+    author: "well",
+    categories: [],
+};
+
+const user = {
+    id: "none",
+    name: "A",
+    bio: " ",
+    facebook: " ",
+    twitter: " ",
+    email: " ",
+    instagram: " ",
+    createdAt: " ",
+    image: " ",
+    ideas: [],
+    point: 100
+}
+
+const IssueDetails: FC<IIdeasDetails> = ({ issue }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const author = user;//useUserByUid(issue.authorId);
+
+  const users = [
+    {
+      name: author?.name || "Unknown",
+      position: "Author",
+      avatar: author?.image || "/images/content/avatar-2.jpg",
+    },
+  ];
+
+
+  return (
+    <>
+      <div className={cn("section", styles.section)}>
+        <div className={cn("container", styles.container)}>
+          <div className={styles.details}>
+            <h1 className={cn("h3", styles.title)}>{issue.title}</h1>
+            <div className={styles.cate}>
+              {issue.categories.map((category: any, index: number) => 
+                <div key={index} className={cn("status-purple", styles.tag)}>
+                  #{category.name}
+                </div>
+              )}
+            </div>
+            {/*Changes */}
+            <h2 className={cn("h3", styles.title)}>Description</h2>
+            <div className={styles.info}>
+              {idea.description}
+            </div>
+            {/* <h2 className={cn("h3", styles.title)}>Criteria</h2>
+            <div className={styles.info}>
+              {idea.}
+            </div> */}
+
+            {/*End*/}
+
+            <div className={styles.nav}>
+              {navLinks.map((x, index) => (
+                <button
+                  className={cn(
+                    { [styles.active]: index === activeIndex },
+                    styles.link
+                  )}
+                  onClick={() => setActiveIndex(index)}
+                  key={index}
+                >
+                  {x}
+                </button>
+              ))}
+            </div>
+            {/* <UserItem className={styles.users} items={users} /> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default IssueDetails;
---
+++ src/components/templates/ideas-details/index.ts
@@ -0,0 +1,1 @@
+export { default as IssueDetails } from "./IdeasDetails";
