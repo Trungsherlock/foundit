fe4a376fe920a83da629af3e6a3a276b67114670
trungnguyen <trungnguyenquochn2202@gmail.com>
2023-03-13 00:36:56 -0400

    create a search web with openai api

--- src/components/mock/bids.js
+++ src/components/mock/bids.js
@@ -1,7 +1,171 @@
-export const bids = [ 
-  { name: "Mint", url: "https://www.mint.com/", image: "https://assets.themuse.com/uploaded/companies/1500/small_logo.png?v=None" }, 
-  { name: "Personal Capital", url: "https://www.personalcapital.com/", image: "https://cdn.personalcapital.com/assets/corp/img/logo/logo_small.svg" }, 
-  { name: "YNAB", url: "https://www.youneedabudget.com/", image: "https://cdn.youneedabudget.com/web-assets-static/images/ynab-logo-square-color.svg" }, 
-  { name: "EveryDollar", url: "https://www.everydollar.com/", image: "https://www.everydollar.com/assets/images/everydollar_square.png" }, 
-  { name: "WalletHub", url: "https://wallethub.com/", image: "https://cdn.creditcards.com/Content/img/master-card-logos/WalletHub-logo.jpg" } 
+export const bids = [
+  {
+    title: "Amazing digital art",
+  price: "2.45 ETH",
+  highestBid: "0.001 ETH",
+  counter: "3 in stock",
+  bid: 'New bid <span role="img" aria-label="fire">🔥</span>',
+  image: "/images/content/card-pic-1.jpg",
+  image2x: "/images/content/card-pic-1@2x.jpg",
+  category: "green",
+  categoryText: "purchasing !",
+  url: "/",
+  users: [
+    {
+      avatar: "/images/content/avatar-1.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-3.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-4.jpg",
+    },
+  ],
+  },
+  {
+  title: "Ribbon Hunter",
+  price: "2.45 ETH",
+  highestBid: "0.001 ETH",
+  counter: "3 in stock",
+  bid: 'New bid <span role="img" aria-label="fire">🔥</span>',
+  image: "/images/content/card-pic-2.jpg",
+  image2x: "/images/content/card-pic-2@2x.jpg",
+  category: "green",
+  categoryText: "purchasing !",
+  url: "/",
+  users: [
+    {
+      avatar: "/images/content/avatar-1.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-4.jpg",
+    },
+  ],
+  },
+  {
+  title: "Amazing digital art",
+  price: "2.45 ETH",
+  highestBid: "0.001 ETH",
+  counter: "3 in stock",
+  bid: 'New bid <span role="img" aria-label="fire">🔥</span>',
+  image: "/images/content/card-pic-3.jpg",
+  image2x: "/images/content/card-pic-3@2x.jpg",
+  category: "green",
+  categoryText: "purchasing !",
+  url: "/",
+  users: [
+    {
+      avatar: "/images/content/avatar-1.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-3.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-4.jpg",
+    },
+  ],
+  },
+  {
+  title: "Amazing digital art",
+  price: "2.45 ETH",
+  highestBid: "0.001 ETH",
+  counter: "3 in stock",
+  bid: 'New bid <span role="img" aria-label="fire">🔥</span>',
+  image: "/images/content/card-pic-4.jpg",
+  image2x: "/images/content/card-pic-4@2x.jpg",
+  category: "green",
+  categoryText: "purchasing !",
+  url: "/",
+  users: [
+    {
+      avatar: "/images/content/avatar-1.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-4.jpg",
+    },
+  ],
+  },
+  {
+  title: "Ribbon Hunter",
+  price: "2.45 ETH",
+  highestBid: "0.001 ETH",
+  counter: "3 in stock",
+  bid: 'New bid <span role="img" aria-label="fire">🔥</span>',
+  image: "/images/content/card-pic-5.jpg",
+  image2x: "/images/content/card-pic-5@2x.jpg",
+  category: "green",
+  categoryText: "purchasing !",
+  url: "/",
+  users: [
+    {
+      avatar: "/images/content/avatar-1.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-3.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-4.jpg",
+    },
+  ],
+  },
+  {
+  title: "Amazing digital art",
+  price: "2.45 ETH",
+  highestBid: "0.001 ETH",
+  counter: "3 in stock",
+  bid: 'New bid <span role="img" aria-label="fire">🔥</span>',
+  image: "/images/content/card-pic-6.jpg",
+  image2x: "/images/content/card-pic-6@2x.jpg",
+  category: "green",
+  categoryText: "purchasing !",
+  url: "/",
+  users: [
+    {
+      avatar: "/images/content/avatar-1.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-4.jpg",
+    },
+  ],
+  },
+  {
+  title: "Amazing digital art",
+  price: "2.45 ETH",
+  highestBid: "0.001 ETH",
+  counter: "3 in stock",
+  bid: 'New bid <span role="img" aria-label="fire">🔥</span>',
+  image: "/images/content/card-pic-7.jpg",
+  image2x: "/images/content/card-pic-7@2x.jpg",
+  category: "green",
+  categoryText: "purchasing !",
+  url: "/",
+  users: [
+    {
+      avatar: "/images/content/avatar-1.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-4.jpg",
+    },
+  ],
+  },
+  {
+  title: "Amazing digital art",
+  price: "2.45 ETH",
+  highestBid: "0.001 ETH",
+  counter: "3 in stock",
+  bid: 'New bid <span role="img" aria-label="fire">🔥</span>',
+  image: "/images/content/card-pic-1.jpg",
+  image2x: "/images/content/card-pic-1@2x.jpg",
+  category: "green",
+  categoryText: "purchasing !",
+  url: "/",
+  users: [
+    {
+      avatar: "/images/content/avatar-1.jpg",
+    },
+    {
+      avatar: "/images/content/avatar-4.jpg",
+    },
+  ],
+  },
 ];
